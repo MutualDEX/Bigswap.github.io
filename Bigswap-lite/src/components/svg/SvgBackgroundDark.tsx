78b218ca956593065cc8f3ca96d8a148bca368b5
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-01-14 14:07:06 -0500

    Add files via upload

---
+++ Bigswap-lite/src/components/svg/SvgBackgroundDark.tsx
@@ -0,0 +1,104 @@
+import * as React from "react";
+import Svg, {
+    Circle,
+    ClipPath,
+    Defs,
+    Ellipse,
+    G,
+    LinearGradient,
+    Path,
+    RadialGradient,
+    Stop,
+    SvgProps
+} from "react-native-svg";
+
+// tslint:disable-next-line:max-func-body-length
+function SvgBackgroundDark(props: SvgProps) {
+    return (
+<Svg width="1083px" height="1467px" viewBox="0 0 1083 1467" {...props} >
+    <G id="page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <G id="pattern-sweet-dots" fill="#FFFFFF">
+            <G id="Group" opacity="0.4">
+                <Circle id="Oval" cx={1.5} cy={1.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={184.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={367.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={550.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={733.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={916.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1099.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1282.5} r={1.5}></Circle>
+                <Ellipse id="Oval" cx={1.5} cy={1424} rx={1.5} ry={2}></Ellipse>
+            </G>
+            <G id="Group" opacity="0.4" transform="translate(180.000000, 0.000000)">
+                <Circle id="Oval" cx={1.5} cy={1.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={184.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={367.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={550.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={733.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={916.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1099.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1282.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1465.5} r={1.5}></Circle>
+            </G>
+            <G id="Group" opacity="0.4" transform="translate(360.000000, 0.000000)">
+                <Circle id="Oval" cx={1.5} cy={1.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={184.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={367.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={550.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={733.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={916.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1099.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1282.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1465.5} r={1.5}></Circle>
+            </G>
+            <G id="Group" opacity="0.4" transform="translate(540.000000, 0.000000)">
+                <Circle id="Oval" cx={1.5} cy={1.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={184.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={367.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={550.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={733.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={916.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1099.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1282.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1465.5} r={1.5}></Circle>
+            </G>
+            <G id="Group" opacity="0.4" transform="translate(720.000000, 0.000000)">
+                <Circle id="Oval" cx={1.5} cy={1.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={184.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={367.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={550.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={733.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={916.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1099.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1282.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1465.5} r={1.5}></Circle>
+            </G>
+            <G id="Group" opacity="0.2" transform="translate(900.000000, 0.000000)">
+                <Circle id="Oval" cx={1.5} cy={1.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={184.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={367.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={550.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={733.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={916.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1099.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1282.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1465.5} r={1.5}></Circle>
+            </G>
+            <G id="Group" opacity="0.1" transform="translate(1080.000000, 0.000000)">
+                <Circle id="Oval" cx={1.5} cy={1.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={184.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={367.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={550.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={733.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={916.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1099.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1282.5} r={1.5}></Circle>
+                <Circle id="Oval" cx={1.5} cy={1465.5} r={1.5}></Circle>
+            </G>
+        </G>
+    </G>
+</Svg>
+    );
+}
+
+export default SvgBackgroundDark;
