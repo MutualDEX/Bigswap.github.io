618a1cbabc4eb57084401920c6abf0eec08355b0
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-01-15 10:40:01 -0500

    Update build_web.sh

--- Bigswap-lite/scripts/build_web.sh
+++ Bigswap-lite/scripts/build_web.sh
@@ -3,6 +3,6 @@
 expo build:web
 web_build_return_code="$?"
 
-echo "mutualdex.github.io/Bigswap.github.io" > web-build/CNAME
+echo "Bigswap.github.io" > web-build/CNAME
 
 exit "${web_build_return_code}"
